76d7bd5196f5e6a5a84c4029c89cc9b971fa358a
Marco <46133335+Marco4413@users.noreply.github.com>
2021-03-17 20:31:20 +0100

    Added instructions DATA, MOD, AND, OR, NOT, XOR
    Added more special characters: \t, \b, \r, \f, \n, \0
    Added Instructions: DATA, MOD, AND, OR, NOT, XOR
    Added predefined constants (All VKs are now available by default)
    
    Added method Processor#updateFlags to update flags based on the given value

---
+++ src/main/java/io/github/hds/pemu/compiler/Constants.java
@@ -0,0 +1,27 @@
+package io.github.hds.pemu.compiler;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+public class Constants {
+
+    private static final HashMap<String, Integer> CONSTANTS = new HashMap<>();
+
+    static {
+        for (Field field : KeyEvent.class.getFields()) {
+            String fieldName = field.getName();
+            if (fieldName.startsWith("VK_"))
+                try {
+                    CONSTANTS.put(fieldName, field.getInt(null));
+                } catch (Exception ignored) { }
+        }
+    }
+
+    public static @NotNull HashMap<String, Integer> getDefaultConstants() {
+        return new HashMap<>(CONSTANTS);
+    }
+
+}
