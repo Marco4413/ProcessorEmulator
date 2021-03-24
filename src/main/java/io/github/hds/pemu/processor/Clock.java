8555d71de87f7cb500ddd06869ba3c7bcaadf661
hds536jhmk <hds536jhmk@gmail.com>
2021-03-24 11:06:29 +0100

    Processor's Clock can now be changed at runtime
    Processor's Clock can now be changed at runtime

--- src/main/java/io/github/hds/pemu/processor/Clock.java
+++ src/main/java/io/github/hds/pemu/processor/Clock.java
@@ -5,20 +5,28 @@ public class Clock {
     public static final int MAX_CLOCK = 1_000_000_000;
     public static final int MIN_CLOCK = 1;
 
-    public final int CLOCK;
-    private final int TIME_BETWEEN_UPDATES;
+    private int clock;
+    private int timeBetweenUpdates;
 
     private long lastUpdated = 0;
 
     public Clock(int clock) {
+        setClock(clock);
+    }
+
+    public void setClock(int clock) {
         if (clock < MIN_CLOCK) throw new IllegalArgumentException("Clock can't be less than " + MIN_CLOCK + "Hz!");
-        CLOCK = clock;
-        TIME_BETWEEN_UPDATES = MAX_CLOCK / CLOCK;
+        this.clock = clock;
+        this.timeBetweenUpdates = MAX_CLOCK / clock;
+    }
+
+    public int getClock() {
+        return clock;
     }
 
     public boolean update() {
         long currentTime = System.nanoTime();
-        if (currentTime - lastUpdated >= TIME_BETWEEN_UPDATES) {
+        if (currentTime - lastUpdated >= timeBetweenUpdates) {
             lastUpdated = currentTime;
             return true;
         }
