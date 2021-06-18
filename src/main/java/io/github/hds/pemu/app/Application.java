5740a3ec112bd526686b35beceb3cef806365dc9
hds536jhmk <hds536jhmk@gmail.com>
2021-06-18 10:24:19 +0200

    Added feature to see where the currently selected cell is pointing at in MemoryView
    Added classes: MemoryTable, MemoryCellRenderer
    Added feature to see where the currently selected cell is pointing at in MemoryView

--- src/main/java/io/github/hds/pemu/app/Application.java
+++ src/main/java/io/github/hds/pemu/app/Application.java
@@ -23,7 +23,7 @@ import java.util.function.Function;
 public class Application extends JFrame implements KeyListener, ITranslatable, IConfigurable {
 
     public static final String APP_TITLE = "PEMU";
-    public static final String APP_VERSION = "1.4.4";
+    public static final String APP_VERSION = "1.5.0";
     public static final int FRAME_WIDTH = 800;
     public static final int FRAME_HEIGHT = 600;
     public static final int FRAME_ICON_SIZE = 32;
